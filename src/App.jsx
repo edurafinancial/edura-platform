bc4eb89d192e0bc1882985283836765d8bfbdbab
sshowmik007 <sadmanshowmik007@gmail.com>
2026-04-03 19:49:22 +0600

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import Error404Page from "./pages/Error404Page";
 import LearningPage from "./pages/LearningPage";
+import DashboardPage from "./pages/DashboardPage";
 
 export default function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<LearningPage />} />
-			<Route path="*" element={<Error404Page />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<LearningPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="*" element={<Error404Page />} />
+    </Routes>
+  );
 }
